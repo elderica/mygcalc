682f35e93ce4991c1304a51c260d9b1686490cff
elderica <shigemichik@gmail.com>
2022-07-19 21:18:09 +0900

    `evalexpr` を使って動く電卓ができた

--- src/app.rs
+++ src/app.rs
@@ -1,36 +1,29 @@
+use evalexpr::eval;
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-
-    // this how you opt-out of serialization of a member
-    #[serde(skip)]
-    value: f32,
+    display: String,
+    // accumulator: f64,
+    // radio: bool,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            display: "0".to_owned(),
+            // accumulator: 0.,
+            // radio: true,
         }
     }
 }
 
 impl TemplateApp {
     /// Called once before the first frame.
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customized the look at feel of egui using
         // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
-
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
-        if let Some(storage) = cc.storage {
-            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
-        }
 
         Default::default()
     }
@@ -38,14 +31,18 @@ impl TemplateApp {
 
 impl eframe::App for TemplateApp {
     /// Called by the frame work to save state before shutdown.
-    fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        eframe::set_value(storage, eframe::APP_KEY, self);
+    fn save(&mut self, _storage: &mut dyn eframe::Storage) {
+        // eframe::set_value(storage, eframe::APP_KEY, self);
     }
 
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        let Self { label, value } = self;
+        let Self {
+            display,
+            // accumulator,
+            // radio,
+        } = self;
 
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
@@ -63,49 +60,162 @@ impl eframe::App for TemplateApp {
             });
         });
 
-        egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
-
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
-            });
-
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to("eframe", "https://github.com/emilk/egui/tree/master/eframe");
-                });
-            });
-        });
-
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
 
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
-            egui::warn_if_debug_build(ui);
-        });
+            ui.vertical_centered_justified(|ui| {
+                ui.text_edit_singleline(display);
+                ui.columns(4, |ui| {
+                    ui[0].vertical_centered_justified(|ui| {
+                        // if ui.add(egui::Button::new("1").frame(true)).clicked() {
+                        if ui.button("1").clicked() {
+                            if *display == "0" {
+                                *display = "1".to_string();
+                            } else {
+                                *display += "1";
+                            }
+                        }
+                        if ui.button("4").clicked() {
+                            if *display == "0" {
+                                *display = "4".to_string();
+                            } else {
+                                *display += "4";
+                            }
+                        }
+                        if ui.button("7").clicked() {
+                            if *display == "0" {
+                                *display = "7".to_string();
+                            } else {
+                                *display += "7";
+                            }
+                        }
+                        if ui.button("0").clicked() {
+                            if *display == "0" {
+                                *display = "0".to_string();
+                            } else {
+                                *display += "0";
+                            }
+                        }
+                        if ui.button("<-").clicked() {
+                            display.pop();
+                            if display.is_empty() {
+                                *display = "0".to_string();
+                            }
+                        }
+                    });
+                    ui[1].vertical_centered_justified(|ui| {
+                        if ui.button("2").clicked() {
+                            if *display == "0" {
+                                *display = "2".to_string();
+                            } else {
+                                *display += "2";
+                            }
+                        }
+                        if ui.button("5").clicked() {
+                            if *display == "0" {
+                                *display = "5".to_string();
+                            } else {
+                                *display += "5";
+                            }
+                        }
+                        if ui.button("8").clicked() {
+                            if *display == "0" {
+                                *display = "8".to_string();
+                            } else {
+                                *display += "8";
+                            }
+                        }
+                        if ui.button(".").clicked() && !display.contains('.') {
+                            *display += ".";
+                        }
+                        if ui.button("C").clicked() {
+                            *display = "0".to_string();
+                        }
+                    });
+                    ui[2].vertical_centered_justified(|ui| {
+                        if ui.button("3").clicked() {
+                            if *display == "0" {
+                                *display = "3".to_string();
+                            } else {
+                                *display += "3";
+                            }
+                        }
+                        if ui.button("6").clicked() {
+                            if *display == "0" {
+                                *display = "6".to_string();
+                            } else {
+                                *display += "6";
+                            }
+                        }
+                        if ui.button("9").clicked() {
+                            if *display == "0" {
+                                *display = "9".to_string();
+                            } else {
+                                *display += "9";
+                            }
+                        }
+                        if ui.button("(").clicked() {
+                            if *display == "0" {
+                                *display = "(".to_string();
+                            } else {
+                                *display += "(";
+                            }
+                        }
+                        if ui.button(")").clicked() {
+                            if *display == "0" {
+                                *display = ")".to_string();
+                            } else {
+                                *display += ")";
+                            }
+                        }
+                    });
+                    ui[3].vertical_centered_justified(|ui| {
+                        if ui.button("+").clicked() {
+                            if *display == "0" {
+                                *display = "+".to_string();
+                            } else {
+                                *display += "+";
+                            }
+                        }
+                        if ui.button("-").clicked() {
+                            if *display == "0" {
+                                *display = "-".to_string();
+                            } else {
+                                *display += "-";
+                            }
+                        }
+                        if ui.button("*").clicked() {
+                            if *display == "0" {
+                                *display = "*".to_string();
+                            } else {
+                                *display += "*";
+                            }
+                        }
+                        if ui.button("/").clicked() {
+                            if *display == "0" {
+                                *display = "/".to_string();
+                            } else {
+                                *display += "/";
+                            }
+                        }
+                        if ui.button("=").clicked() {
+                            if let Ok(v) = eval(display) {
+                                *display = v.to_string();
+                            } else {
+                                *display = "ERROR".to_string();
+                            }
+                        }
+                    });
+                });
 
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
+                // egui::ComboBox::from_label("")
+                //     .selected_text(format!("{:?}", radio))
+                //     .width(320.)
+                //     .show_ui(ui, |ui| {
+                //         ui.selectable_value(radio, true, "true");
+                //         ui.selectable_value(radio, false, "false");
+                //     });
             });
-        }
+        });
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,18 @@
 // When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
+    use egui::Vec2;
+
     // Log to stdout (if you run with `RUST_LOG=debug`).
     tracing_subscriber::fmt::init();
 
-    let native_options = eframe::NativeOptions::default();
+    // let mut native_options = eframe::NativeOptions::default();
+    let winsize = Vec2::new(800., 800.);
+    let native_options = eframe::NativeOptions {
+        initial_window_size: Some(winsize),
+        max_window_size: Some(winsize),
+        ..eframe::NativeOptions::default()
+    };
     eframe::run_native(
         "mygcalc",
         native_options,
